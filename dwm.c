a6650e3fbae3adbd1e4df6eef1baa207ad5906b4
sin <sin@2f30.org>
2018-06-28 17:40:40 +0100

    dwmfifo
    This patch adds support for using a command/control FIFO for dwm.  I've
    added commands that map 1-1 with the existing keybind actions.  You can
    use this patch to script dwm.  As an example the following sequence of
    commands starts 2 terminals on each of the 2 monitors.
    
    echo term > /tmp/dwm.fifo
    sleep 0.5
    echo term > /tmp/dwm.fifo
    sleep 0.5
    echo focusmon+ > /tmp/dwm.fifo
    sleep 0.5
    echo term > /tmp/dwm.fifo
    sleep 0.5
    echo term > /tmp/dwm.fifo
    
    The sleep in between is currently needed to avoid buffering up
    more than a single command.  You may experiment with the actual
    sleep value.
    
    Similarly you can modify your config.h and add more commands
    that you may want to execute (like tabbed-surf or similar).
    
    URL: http://dwm.suckless.org/patches/dwmfifo/dwm-dwmfifo-6.1.diff

--- dwm.c
+++ dwm.c
@@ -21,6 +21,7 @@
  * To understand everything else, start reading main().
  */
 #include <errno.h>
+#include <fcntl.h>
 #include <locale.h>
 #include <signal.h>
 #include <stdarg.h>
@@ -28,6 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <X11/cursorfont.h>
@@ -141,6 +143,12 @@ typedef struct {
 	int monitor;
 } Rule;
 
+typedef struct {
+	const char *name;
+	void (*func)(const Arg *arg);
+	const Arg arg;
+} Command;
+
 /* function declarations */
 static void applyrules(Client *c);
 static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact);
@@ -161,9 +169,11 @@ static void destroynotify(XEvent *e);
 static void detach(Client *c);
 static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
+static void dispatchcmd(void);
 static void drawbar(Monitor *m);
 static void drawbars(void);
 static void enternotify(XEvent *e);
+static Bool evpredicate();
 static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
@@ -267,6 +277,7 @@ static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
 static Window root, wmcheckwin;
+static int fifofd;
 
 /* configuration, allows nested code to access above variables */
 #include "config.h"
@@ -491,6 +502,7 @@ cleanup(void)
 	XSync(dpy, False);
 	XSetInputFocus(dpy, PointerRoot, RevertToPointerRoot, CurrentTime);
 	XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
+	close(fifofd);
 }
 
 void
@@ -695,6 +707,26 @@ dirtomon(int dir)
 }
 
 void
+dispatchcmd(void)
+{
+	int i;
+	char buf[BUFSIZ];
+	ssize_t n;
+
+	n = read(fifofd, buf, sizeof(buf) - 1);
+	if (n == -1)
+		die("Failed to read() from DWM fifo %s:", dwmfifo);
+	buf[n] = '\0';
+	buf[strcspn(buf, "\n")] = '\0';
+	for (i = 0; i < LENGTH(commands); i++) {
+		if (strcmp(commands[i].name, buf) == 0) {
+			commands[i].func(&commands[i].arg);
+			break;
+		}
+	}
+}
+
+void
 drawbar(Monitor *m)
 {
 	int x, w, sw = 0;
@@ -770,6 +802,12 @@ enternotify(XEvent *e)
 	} else if (!c || c == selmon->sel)
 		return;
 	focus(c);
+}
+
+Bool
+evpredicate()
+{
+	return True;
 }
 
 void
@@ -1375,11 +1413,30 @@ void
 run(void)
 {
 	XEvent ev;
+	fd_set rfds;
+	int n;
+	int dpyfd, maxfd;
 	/* main event loop */
 	XSync(dpy, False);
-	while (running && !XNextEvent(dpy, &ev))
-		if (handler[ev.type])
-			handler[ev.type](&ev); /* call handler */
+	dpyfd = ConnectionNumber(dpy);
+	maxfd = fifofd;
+	if (dpyfd > maxfd)
+		maxfd = dpyfd;
+	maxfd++;
+	while (running) {
+		FD_ZERO(&rfds);
+		FD_SET(fifofd, &rfds);
+		FD_SET(dpyfd, &rfds);
+		n = select(maxfd, &rfds, NULL, NULL, NULL);
+		if (n > 0) {
+			if (FD_ISSET(fifofd, &rfds))
+				dispatchcmd();
+			if (FD_ISSET(dpyfd, &rfds))
+				while (XCheckIfEvent(dpy, &ev, evpredicate, NULL))
+					if (handler[ev.type])
+						handler[ev.type](&ev); /* call handler */
+		}
+	}
 }
 
 void
@@ -1531,6 +1588,7 @@ setmfact(const Arg *arg)
 void
 setup(void)
 {
+	struct stat sb;
 	int i;
 	XSetWindowAttributes wa;
 	Atom utf8string;
@@ -1596,6 +1654,11 @@ setup(void)
 	XSelectInput(dpy, root, wa.event_mask);
 	grabkeys();
 	focus(NULL);
+	if(stat(dwmfifo, &sb) == -1)
+		mkfifo(dwmfifo, S_IRUSR | S_IWUSR);
+	fifofd = open(dwmfifo, O_RDWR | O_NONBLOCK);
+	if (fifofd < 0)
+		die("Failed to open() DWM fifo %s:", dwmfifo);
 }
 
 
