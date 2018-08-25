c16f937f728bfe75b46e78bb9c8581490371aef7
James Rowe <jnrowe@gmail.com>
2018-08-25 06:54:25 +0100

    Support changing bar border size

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const char urgbordercolor[]  = "#ff0000";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int font_border = 2;      /* border around bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
