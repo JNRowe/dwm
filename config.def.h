270a5536fcd3e909b5a7b201102dfeb63ffdc9dc
Petr Schmied <7877707+JBlackN@users.noreply.github.com>
2019-10-07 19:55:56 +0100

    xrdb
    Allows ``dwm`` to read colors from ``.Xresources`` at run time.
    
    X resources used:
    
    * ``dwm.normbordercolor``
    * ``dwm.normbgcolor``
    * ``dwm.normfgcolor``
    * ``dwm.selbordercolor``
    * ``dwm.selbgcolor``
    * ``dwm.selfgcolor``
    
    Example Xresource string: ``dwm.normbgcolor: #002b36``.
    
    Default key to reload is ``MODKEY-F5``.
    
    URL: http://dwm.suckless.org/patches/xrdb/dwm-xrdb-6.2.diff

--- config.def.h
+++ config.def.h
@@ -10,15 +10,16 @@ static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const Bool scratchpad_centre = False;    /* Centre scratchpad vertically */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][4]      = {
-	/*               fg         bg         border          float*/
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2,      col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2,      col_cyan },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][4] = {
+	/*               fg           bg           border           float*/
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor,  selbgcolor },
 };
 
 /* tagging */
@@ -66,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname,
@@ -108,6 +109,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	{ MODKEY,                       XK_n,      nametag,        {0} },
 	{ MODKEY,                       XK_q,      moveplace,      {.ui = WIN_NW }},
 	{ MODKEY,                       XK_w,      moveplace,      {.ui = WIN_N  }},
